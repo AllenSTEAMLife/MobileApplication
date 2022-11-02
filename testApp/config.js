74aafa1d16c71e715782731126fcfbfbfb08fd94
Matthew C <69691791+roboticsDev1584@users.noreply.github.com>
2022-11-01 19:51:24 -0500

    Delete config.js

--- testApp/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyCSpKcE-kBL1ksbK4hHcwO-16lPN7OW7oE",
-    authDomain: "steam-life-mobile-auth.firebaseapp.com",
-    projectId: "steam-life-mobile-auth",
-    storageBucket: "steam-life-mobile-auth.appspot.com",
-    messagingSenderId: "124067043994",
-    appId: "1:124067043994:web:3bd93e3d933b17af54dd23",
-    measurementId: "G-PRZGJHN70S"
-  };
