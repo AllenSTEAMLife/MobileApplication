0e74892c009f9f8c3c65f0339f8fdc67dc3d1d82
roboticsDev1584 <matthewcalda@gmail.com>
2023-01-01 13:41:48 -0600

    Added settings header

---
+++ testApp/scenes/headers/SettingsHeader.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native';
+import {ViewPropTypes} from 'deprecated-react-native-prop-types';
+
+const Header = () => {
+    return (
+        <View style={{ alignItems: 'center' }}>
+            <View style={{ alignItems: 'center', flexDirection: 'row', height: 28 }}>
+
+            </View>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    
+});
+export default Header;
