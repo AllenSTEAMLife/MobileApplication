338d7f5846df5fc74dbef2171367e929bf0c07ee
roboticsDev1584 <matthewcalda@gmail.com>
2023-01-01 15:59:57 -0600

    Fixed home page header font size for Android

--- testApp/scenes/headers/HomePageHeader.js
+++ testApp/scenes/headers/HomePageHeader.js
@@ -7,32 +7,34 @@ const HomePageHeader = () => {
     return (
         <View style={{flex: 1, alignItems: 'center' }}>
             <View style={{alignItems: 'center', flexDirection: 'row'}}>
-                
                 <Text style={styles.titleText}>
-                    <Text style={{color: Colors.GREEN}}>
+                    <Text style={[styles.subTitleText,{color: Colors.GREEN}]}>
                     S
                     </Text>
-                    <Text style={{color: Colors.BLUE}}>
+                    <Text style={[styles.subTitleText,{color: Colors.BLUE}]}>
                     T
-                    </Text><Text style={{color: Colors.PURPLE}}>
+                    </Text><Text style={[styles.subTitleText,{color: Colors.PURPLE}]}>
                     E
-                    </Text><Text style={{color: Colors.YELLOW}}>
+                    </Text><Text style={[styles.subTitleText,{color: Colors.YELLOW}]}>
                     A
-                    </Text><Text style={{color: Colors.RED}}>
+                    </Text><Text style={[styles.subTitleText,{color: Colors.RED}]}>
                     M 
                     </Text>
                     <Text style={{color: Colors.BLACK, fontFamily: 'Montserrat-Medium'}}>
                     {' LIFE'}
                     </Text>
                 </Text>
-                
             </View>
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                <View style={{flex: 1, height: 2, backgroundColor: Colors.GRAY, marginTop: 6, marginLeft: 10, marginRight: 10}} />
-            </View>
+            
         </View>
     )
 }
+
+/*
+<View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <View style={{flex: 1, height: 2, backgroundColor: Colors.GRAY, marginTop: 6, marginLeft: 10, marginRight: 10}} />
+            </View>
+*/
 
 export default HomePageHeader
 
@@ -42,8 +44,10 @@ const styles = StyleSheet.create({
       },
       titleText: {
           marginTop: 50,
-          fontSize: 36,
+          fontSize: 24,
           fontFamily: "Montserrat-ExtraBold"
-          
+      },
+      subTitleText: {
+        fontSize: 32,
       }
 })
